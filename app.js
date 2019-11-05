e897f4a91476b888e3ab64a086299c861da228b6
the_sociophobic <torikowashi@gmail.com>
2019-11-05 21:50:12 +0300

    static videos

--- app.js
+++ app.js
@@ -10,12 +10,55 @@ app.get('/net-script', function(req, res) {
 app.get('/net-style', function(req, res) {
   res.sendFile(path.join(__dirname, 'parcel/dist', 'index.css'));
 });
-app.get('/net-loader', function(req, res) {
-  res.sendFile(path.join(__dirname, 'parcel/public/static/media', 'loader.gif'));
-});
-app.get('/net-easteregg-music', function(req, res) {
-  res.sendFile(path.join(__dirname, 'parcel/public/static/media', 'Boards of Canada-Turquoise Hexagon Sun.mp3'));
-});
+
+[
+  {
+    url: "loader",
+    fileName: "loader.gif",
+  },
+  {
+    url: "easteregg-music",
+    fileName: "Boards of Canada-Turquoise Hexagon Sun.mp3",
+  },
+  {
+    url: "brook-video",
+    fileName: "brook",
+  },
+  {
+    url: "espinoza-video",
+    fileName: "espinoza",
+  },
+  {
+    url: "mitchel-video",
+    fileName: "mitchel",
+  },
+  {
+    url: "rau-video",
+    fileName: "rau",
+  },
+  {
+    url: "dimchev-video",
+    fileName: "dimchev",
+  },
+  {
+    url: "liddel-video",
+    fileName: "liddel",
+  },
+  {
+    url: "ukhanov-video",
+    fileName: "ukhanov",
+  },
+  {
+    url: "signa-video",
+    fileName: "signa",
+  },
+].forEach(file => app
+  .get("/net-" + file.url,
+    (req, res) =>
+      res.sendFile(path.join(__dirname, 'parcel/public/static/media', file.fileName))
+  )
+)
+
 app.get('/*', function(req, res) {
   res.send("NET festival 3d widget files on this port");
 });
